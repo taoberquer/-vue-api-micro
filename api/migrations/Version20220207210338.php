8e1abc6e2acadd090d84fa65a0e525df7c9c7c2f
Taopol BERQUER <tao.berquer@gmail.com>
2022-02-07 22:11:14 +0100

    Ajout des migrations

--- api/migrations/Version20220207210338.php
+++ api/migrations/Version20220207210338.php
@@ -21,11 +21,7 @@ final class Version20220207210338 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE "order_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE piece_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE "order" (id INT NOT NULL, status VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, zip_code VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE piece (id INT NOT NULL, quality INT NOT NULL, type VARCHAR(255) NOT NULL, color VARCHAR(255) NOT NULL, size VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, damage VARCHAR(255) NOT NULL, sale BOOLEAN NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
@@ -33,10 +29,6 @@ final class Version20220207210338 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE "order_id_seq" CASCADE');
-        $this->addSql('DROP SEQUENCE piece_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
         $this->addSql('DROP TABLE "order"');
-        $this->addSql('DROP TABLE piece');
-        $this->addSql('DROP TABLE "user"');
     }
 }
