30dd7ec7f5541ee0b37e01e28f54aa0c75a5eb22
LudovicCollignon <lud.collignon@gmail.com>
2022-02-14 16:34:27 +0100

    fix derniere minute

--- appvuejs/src/routes/index.js
+++ appvuejs/src/routes/index.js
@@ -9,7 +9,7 @@ export default createRouter({
     {path: "/items", component: () => import("../views/ListItems.vue")},
     {path: "/items/:id", component: () => import("../views/ItemView.vue")},
     {path: "/categories/", component: () => import("../views/CategoriesView.vue")},
-    {path: "/categories/:name", component: () => import("../views/ListItems.vue")},
+    {path: "/categories/:id", component: () => import("../views/ListItems.vue")},
     {path: "/admin/donations", component: () => import("../views/Admin/Donations.vue")},
     {path: "/admin/categories", component: () => import("../views/Admin/Categories.vue")},
     {path: "/payment", component: () => import("../views/Payment.vue")},
